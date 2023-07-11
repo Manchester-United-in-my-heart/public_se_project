d3b6582bbba0e47673355b028cc16eda849021b1
Manchester-United-in-my-heart <129467586+BuiDuongKham@users.noreply.github.com>
2023-07-11 22:06:41 +0700

    fix mistaken url

--- layout/fragments/productList.js
+++ layout/fragments/productList.js
@@ -9,7 +9,7 @@ export default function ProductFragment(props)
       </div>
       {productList && productList.map(product => (
         <div key={product._id} className={''}>
-          <ProductCard productImage={product.productImage} productName={product.productName} productDescription={product.productDescription} productPrice={product.productPrice} productUnit={product.productUnit} productLink={`/${product._id}`}/>
+          <ProductCard productImage={product.productImage} productName={product.productName} productDescription={product.productDescription} productPrice={product.productPrice} productUnit={product.productUnit} productLink={`/product/${product._id}`}/>
         </div>))}
       <div className={'flex justify-center mb-4'}>
         <a className={'px-6 py-3 border-black border-[1px] rounded-2xl hover:bg-blue-600 hover:text-white transition-all duration-300'} href={addUrl}> Thêm sản phẩm </a>
